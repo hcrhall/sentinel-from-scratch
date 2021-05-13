e50dffd9f7bba957cc1c205ed102709b8eb2eb26
Ryan Hall <36720823+hcrhall@users.noreply.github.com>
2021-05-13 14:18:38 +1000

    Update variables.tf

--- 09-debugging-triage/config/variables.tf
+++ 09-debugging-triage/config/variables.tf
@@ -2,5 +2,4 @@ variable "fake_token" {
     type = string
     sensitive = true
     description = "The Terraform Cloud/Enterprise user/team token that will be used in the provider configuration block."
-    default = "750e8fee-85e5-11eb-8dcd-0242ac130003"
 }
