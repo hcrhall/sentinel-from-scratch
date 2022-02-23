91c08d345b4867664fd50f9c44767105581cdf00
Ryan Hall <36720823+hcrhall@users.noreply.github.com>
2022-02-23 13:31:24 +1300

    Update main.tf

--- 09-debugging-triage/config/main.tf
+++ 09-debugging-triage/config/main.tf
@@ -9,17 +9,17 @@ provider "fakewebservices" {
 #
  # Resources
 #
-resource "fakewebservices_vpc" "vpc" {
-  name = "Primary VPC"
-  cidr_block = "0.0.0.0/1"
-}
+# resource "fakewebservices_vpc" "vpc" {
+#   name = "Primary VPC"
+#   cidr_block = "0.0.0.0/1"
+# }
 
 resource "fakewebservices_server" "server" {
   count = var.server_count
 
   name = "Server ${count.index+1}"
   type = "t2.micro"
-  vpc = fakewebservices_vpc.vpc.name
+#   vpc = fakewebservices_vpc.vpc.name
 }
 
 resource "fakewebservices_load_balancer" "load_balancer" {
@@ -27,7 +27,7 @@ resource "fakewebservices_load_balancer" "load_balancer" {
   servers = fakewebservices_server.server[*].name
 }
 
-resource "fakewebservices_database" "database" {
-  name = "Production DB"
-  size = 256
-}
+# resource "fakewebservices_database" "database" {
+#   name = "Production DB"
+#   size = 256
+# }
